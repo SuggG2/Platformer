4c89013fdca8eb15c3c3e41e1d037e8cd51b990f
SuggG2 <hammce16@kingshigh.school.nz>
2022-10-31 12:05:21 +1300

    Menu

---
+++ Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+	
+	
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
